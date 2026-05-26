2d075eb9440a9ce891f6cdf13caedd65fa216db5
ralphtacconi <tacconi.ralph@gmail.com>
2026-05-26 18:07:10 -0300

    sa name2

--- infra/container-apps/backend.tf
+++ infra/container-apps/backend.tf
@@ -8,7 +8,7 @@
 #
 terraform {
   backend "azurerm" {
-    resource_group_name  = "azurerm_resource_group.bbmetrics.name"
+    resource_group_name  = azurerm_resource_group.bbmetrics.name
     storage_account_name = "bbmetricscontainer"
     container_name       = "tfstate"
     key                  = "container-apps/terraform.tfstate"
